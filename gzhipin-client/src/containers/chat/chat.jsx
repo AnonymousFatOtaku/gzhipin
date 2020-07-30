4f9ec2297d51f477e68102cabd79a683518ccffb
damu <damu@example.com>
2020-07-30 19:21:47 +0800

    表情功能

--- gzhipin-client/src/containers/chat/chat.jsx
+++ gzhipin-client/src/containers/chat/chat.jsx
@@ -1,6 +1,6 @@
 // 对话聊天的路由组件
 import React, {Component} from 'react'
-import {NavBar, List, InputItem} from 'antd-mobile'
+import {NavBar, List, InputItem, Grid, Icon} from 'antd-mobile'
 import {connect} from 'react-redux'
 
 import {sendMsg} from '../../redux/actions'
@@ -11,6 +11,7 @@ class Chat extends Component {
 
   state = {
     content: '',
+    isShow: false // 是否显示表情列表
   }
 
   handleSend = () => {
@@ -27,6 +28,37 @@ class Chat extends Component {
       content: '',
       isShow: false
     })
+  }
+
+  toggleShow = () => {
+    const isShow = !this.state.isShow
+    this.setState({isShow})
+    if (isShow) {
+      // 异步手动派发resize事件来解决表情列表显示的bug
+      setTimeout(() => {
+        window.dispatchEvent(new Event('resize'))
+      }, 0)
+    }
+  }
+
+  // 在第一次render()之前回调
+  componentWillMount() {
+    // 初始化表情列表数据
+    const emojis = ['😀', '😁', '🤣', '😀', '😁', '🤣', '😀', '😁', '🤣', '😀', '😁', '🤣', '😀'
+      , '😁', '🤣', '😀', '😁', '🤣', '😀', '😁', '🤣', '😀', '😁', '🤣'
+      , '😁', '🤣', '😀', '😁', '🤣', '😀', '😁', '🤣', '😀', '😁', '🤣'
+      , '😁', '🤣', '😀', '😁', '🤣', '😀', '😁', '🤣', '😀', '😁', '🤣']
+    this.emojis = emojis.map(emoji => ({text: emoji}))
+  }
+
+  componentDidMount() {
+    // 初始显示列表
+    window.scrollTo(0, document.body.scrollHeight)
+  }
+
+  componentDidUpdate() {
+    // 更新显示列表
+    window.scrollTo(0, document.body.scrollHeight)
   }
 
   render() {
@@ -50,8 +82,11 @@ class Chat extends Component {
 
     return (
       <div id='chat-page'>
-        <NavBar>聊天对象</NavBar>
-        <List>
+        <NavBar icon={<Icon type='left'/>} className='sticky-header'
+                onLeftClick={() => this.props.history.goBack()}>
+          {users[targetId].username}
+        </NavBar>
+        <List style={{marginTop: 50, marginBottom: 50}}>
           {
             msgs.map(msg => {
               if (targetId === msg.from) { // 对方发给我的
@@ -71,9 +106,22 @@ class Chat extends Component {
           }
         </List>
         <div className='am-tab-bar'>
+          {
+            this.state.isShow ? (
+              <Grid data={this.emojis} columnNum={8} carouselMaxRow={4} isCarousel={true}
+                    onClick={(item) => {
+                      this.setState({content: this.state.content + item.text})
+                    }}
+              />
+            ) : null
+          }
           <InputItem placeholder="请输入" value={this.state.content} onChange={val => this.setState({content: val})}
+                     onFocus={() => this.setState({isShow: false})}
                      extra={
-                       <span onClick={this.handleSend}>发送</span>
+                       <span>
+                         <span onClick={this.toggleShow} style={{marginRight: 5}}>😊</span>
+                         <span onClick={this.handleSend}>发送</span>
+                       </span>
                      }/>
         </div>
       </div>
